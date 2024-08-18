03153904a2940906c645d3b4b473c632a7f9059b
CrazyEnvelope <stanciustefan500@gmail.com>
2024-08-19 00:16:16 +0300

    Save the input text and repair timer.

--- gui_window.py
+++ gui_window.py
@@ -27,16 +27,16 @@ class guiWindow(tk.Tk):
         labelTitle = tk.Label(menuFrame, text="TST", font=("Benett Sans Serif", 30, 'bold'), fg="white", bg="#402E7A")
         labelTitle.grid(row=0, column=0, pady=5, padx=(0,100), sticky="w")
 
-        labelWPM = tk.Label(menuFrame,text ="WPM:" , font = ("MS Sans Serif", 10, 'normal') ,fg="white", bg="#402E7A")
+        labelWPM = tk.Label(menuFrame,text ="WPM: 0" , font = ("MS Sans Serif", 10, 'normal') ,fg="white", bg="#402E7A")
         labelWPM.grid(row = 1, column = 0, pady = (60,5), sticky="w")
 
-        labelErrors = tk.Label(menuFrame, text = "Entry errors:" , font=("MS Sans Serif", 10, 'normal'),fg="white",bg="#402E7A")
+        labelErrors = tk.Label(menuFrame, text = "Entry errors: 0" , font=("MS Sans Serif", 10, 'normal'),fg="white",bg="#402E7A")
         labelErrors.grid(row=2, column=0, pady = 5, sticky="w")
 
-        labelAccuracy = tk.Label(menuFrame,text = "Accuracy:", font=("MS Sans Serif", 10, 'normal'),fg="white",bg="#402E7A")
+        labelAccuracy = tk.Label(menuFrame,text = "Accuracy: 0 %", font=("MS Sans Serif", 10, 'normal'),fg="white",bg="#402E7A")
         labelAccuracy.grid(row=3, column=0, pady = 5, sticky="w")
 
-        self.labelTime = tk.Label(menuFrame, text = "Time remaining:", font=("MS Sans Serif", 10, 'normal'), fg="white",bg="#402E7A")
+        self.labelTime = tk.Label(menuFrame, text = "Time remaining: 00 s", font=("MS Sans Serif", 10, 'normal'), fg="white",bg="#402E7A")
         self.labelTime.grid(row=4, column=0, pady = 5, sticky="w")
 
         menuFrame.grid_rowconfigure(5,weight = 1)
--- main.py
+++ main.py
@@ -3,8 +3,9 @@ from text import  paragraphs
 import random
 
 timer_id = None
+is_finished = False
 
-def startTimer(seconds=60):
+def startTimer(seconds=5):
     global timer_id
     if seconds == 60:
         secs = "60"
@@ -12,21 +13,44 @@ def startTimer(seconds=60):
         mins, secs = divmod(seconds,60)
         if secs < 10:
             secs = "0" + str(secs)
-    guiSpeedtest.labelTime.config(text = f"Time remaining: {secs}")
+    guiSpeedtest.labelTime.config(text = f"Time remaining: {secs} s")
     timer_id = guiSpeedtest.after(1000, startTimer, seconds-1)
 
+    if int(secs) < 1:
+        stopTimer()
+        guiSpeedtest.inputText.config(state="disabled", bg="#F7F7F8")
+
 def startTyping():
+    global is_finished
+    is_finished = False
     randomParagraph = random.choice(paragraphs)
     guiSpeedtest.randomText.config(text = randomParagraph)
     guiSpeedtest.inputText.config(state="normal", bg = "white")
+    guiSpeedtest.inputText.delete("1.0","end")
+    stopTimer()
     startTimer()
 
 def stopTyping():
-    global timer_id
+    stopTimer()
+    guiSpeedtest.inputText.config(state="disabled", bg="#F7F7F8")
+    guiSpeedtest.labelTime.config(text=f"Time remaining: 00")
+
+def stopTimer():
+    global timer_id, is_finished
     if timer_id is not None:
         guiSpeedtest.after_cancel(timer_id)
         timer_id = None
-    guiSpeedtest.inputText.config(state="disabled", bg="#F7F7F8")
+        is_finished = True
+    calculateErrors()
+
+def calculateErrors():
+    global is_finished
+    if is_finished:
+        content = guiSpeedtest.inputText.get("1.0","end")
+        words_written = [word for word in content.split(" ")]
+        words_written[-1] = words_written[-1].strip()
+
+        print(words_written)
 
 guiSpeedtest = guiWindow()
 guiSpeedtest.inputText.config(state = "disabled", bg = "#F7F7F8")
